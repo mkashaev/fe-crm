1da6291778bd559c71d62a1e020409c796ab24b6
Marat Kashaev <marat@gomagis.com>
2020-07-10 18:34:20 +0300

    ADD auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layout/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      { path: '', redirectTo: '/login', pathMatch: 'full'},
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent },
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, children: []
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
